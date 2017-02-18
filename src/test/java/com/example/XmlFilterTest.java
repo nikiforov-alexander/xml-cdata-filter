d8981f377738bcd57e3168e4df81e662791ae691
Alexander Nikiforov <nikiforo.alexander@gmail.com>
2017-02-18 20:49:15 +0100

    Add integration test for filter method
    method checks that replacement occurs in all CDATA blocks

--- src/test/java/com/example/XmlFilterTest.java
+++ src/test/java/com/example/XmlFilterTest.java
@@ -8,6 +8,9 @@ import org.w3c.dom.Node;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -33,8 +36,8 @@ public class XmlFilterTest {
 
     // helpful private methods to set up test environment
 
-    private Node getFirstNodeWithBadCharacter() {
-        return document.getElementsByTagName("firstName").item(0);
+    private Node getNodeWithElementAtIndex(String elementName, int index) {
+        return document.getElementsByTagName(elementName).item(index);
     }
 
     // tests
@@ -42,7 +45,7 @@ public class XmlFilterTest {
     @Test
     public void replaceBadEntities_canReplaceNodeOfTheDocument() throws Exception {
         // Given the Node with bad character
-        Node node = getFirstNodeWithBadCharacter();
+        Node node = getNodeWithElementAtIndex("firstName", 0);
         assertThat(node.getTextContent()).contains("&apos;");
 
         // When we call replaceBadEntities with that Node and given Map
@@ -50,5 +53,35 @@ public class XmlFilterTest {
 
         // Then we should get the Node replaced
         assertThat(changedNode.getTextContent()).doesNotContain("&apos;");
+    }
+
+    // integration test
+
+    @Test
+    public void filter_replacesTheBadNodesAndReturnsCorrectDocument() throws Exception {
+        // Given the Document with 4 bad CDATA groups
+        List<Node> badNodes = Arrays.asList(
+                getNodeWithElementAtIndex("firstName", 0),
+                getNodeWithElementAtIndex("lastName", 0),
+                getNodeWithElementAtIndex("nickname", 0),
+                getNodeWithElementAtIndex("salary", 0)
+        );
+        for (Node node: badNodes) {
+            assertThat(node.getTextContent()).contains("&apos;");
+        }
+
+        // When we call filter()
+        xmlFilter.filter();
+
+        // Then 4 nodes should not contain bad characters
+        badNodes = Arrays.asList(
+            getNodeWithElementAtIndex("firstName", 0),
+            getNodeWithElementAtIndex("lastName", 0),
+            getNodeWithElementAtIndex("nickname", 0),
+            getNodeWithElementAtIndex("salary", 0)
+        );
+        for (Node node: badNodes) {
+            assertThat(node.getTextContent()).doesNotContain("&apos;");
+        }
     }
 }
